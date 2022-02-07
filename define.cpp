2958c6b32dbdbd01a0e662cdd8d611103ee0066b
TienDatDao <94846751+TienDatDao@users.noreply.github.com>
2022-02-07 19:52:50 +0700

    Create define.cpp

---
+++ define.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAXN 1000000
+#define ll long long
+#define ull unsigned long long
+#define X first
+#define Y second
+#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    
+	return 0;
+}
