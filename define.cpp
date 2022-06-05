842330dc7ddc9c102c1ebe174b60cffae33cb2da
TienDatDao <94846751+TienDatDao@users.noreply.github.com>
2022-06-05 22:03:56 +0700

    Update define.cpp

--- define.cpp
+++ define.cpp
@@ -1,14 +1,19 @@
+/**
+ * author: name
+ * created: dd/mm/yy
+ * link: 
+**/
 #include<bits/stdc++.h>
 using namespace std;
 
 #define MAXN 1000000
 #define ll long long
-#define ull unsigned long long
 #define X first
 #define Y second
-#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 int main(){
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); cout.tie(NULL);
     	//freopen("main.inp","r",stdin);
     	//freopen("main.out","w",stdout);
 	
